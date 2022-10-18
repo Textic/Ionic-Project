6d0d40dd4fe864f7363a1a12a34ef0616ad9c63c
Textic <danielbienchido@gmail.com>
2022-10-18 15:19:01 -0300

    fixed (again) login ts and login service

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -7,7 +7,6 @@ import { iDriverData, iUserData } from '../others/interfaces/interface';
 import { take } from 'rxjs/operators';
 import { Http, HttpResponse } from '@capacitor-community/http'
 import { LoginService } from '../others/services/login.service';
-// import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -65,6 +64,8 @@ export class LoginPage implements OnInit {
 
   loginData: any
 
+  response: HttpResponse;
+
   loading: any;
   img: string;
   void: String = "";
@@ -101,109 +102,99 @@ export class LoginPage implements OnInit {
     this.makeUserData.mail = this.data.mail
     if (this.validateModel(this.data)) {
       await this.loading.present();
-      this.login.loginCheck().pipe(take(1)).subscribe(async (e: any) => {
-        const length = e.alumnos.length;
-        var userMail = this.data.mail.split('@duocuc.cl')[0];
-        for (let i = 0; i < length; i++) {
-          var username = e.alumnos[i].username;
-          if (username == userMail && e.alumnos[i].password == this.data.password) {
-            if (this.data.mail.includes("@duocuc.cl")) {
-              this.tempLoginData.mail = this.data.mail;
-            } else {
-              this.tempLoginData.mail = this.data.mail + "@duocuc.cl"
-            }
-            this.tempLoginData.name = e.alumnos[i].nombre;
-            this.tempLoginData.boolean = true;
-            if (this.tempLoginData.boolean == true) {
-              this.loginCheckData = {
-                mail: this.tempLoginData.mail,
-                name: this.tempLoginData.name,
-              }
-              this.firestore.getCollectionById<iUserData>("Users", this.loginCheckData.mail).pipe(take(1)).subscribe(e => {
-                if (!e) {
-                  this.makeUserData.name = this.loginCheckData.name.substring(0, this.loginCheckData.name.indexOf(" "));
-                  this.makeUserData.lName = this.loginCheckData.name.substring(this.loginCheckData.name.indexOf(" ") + 1);
-                  this.makeUserData.mail = this.loginCheckData.mail;
-                  this.firestore.setCollection("Users", this.loginCheckData.mail, this.makeUserData)
-                  localStorage.setItem('userMail', this.loginCheckData.mail)
-                  localStorage.setItem('userName', this.loginCheckData.name.substring(0, this.loginCheckData.name.indexOf(" ")))
-                  localStorage.setItem('userLName', this.loginCheckData.name.substring(this.loginCheckData.name.indexOf(" ") + 1))
-                }
-                if (e.mail) {
-                  localStorage.setItem('userMail', e.mail)
-                }
-                if (e.name) {
-                  localStorage.setItem('userName', e.name)
-                }
-                if (e.lName) {
-                  localStorage.setItem('userLName', e.lName)
-                }
-                if (e.number) {
-                  localStorage.setItem('userNumber', e.number)
-                }
-                this.service.presentToast("Sesion Iniciada con el email: " + this.data.mail, 'middle');
-              })
-              this.firestore.getCollectionById<iDriverData>("Drivers", this.loginCheckData.mail).pipe(take(1)).subscribe(e => {
-                if (!e) {
-                  this.firestore.setCollection("Drivers", this.loginCheckData.mail, this.makeDriverData)
-                }
-                if (e.vehicle) {
-                  localStorage.setItem('driverVehicle', e.vehicle)
-                }
-                if (e.capacity) {
-                  localStorage.setItem('driverCapacity', e.capacity)
-                }
-                if (e.patent) {
-                  localStorage.setItem('driverPatent', e.patent)
-                }
-                if (e.time) {
-                  localStorage.setItem('driverTime', e.time)
-                }
-                if (e.locationName) {
-                  localStorage.setItem('driverLocationName', e.locationName)
-                }
-                if (e.name) {
-                  localStorage.setItem('driverName', e.name)
-                }
-                if (e.lName) {
-                  localStorage.setItem('driverLName', e.lName)
-                }
-                if (e.mail) {
-                  localStorage.setItem('driverMail', e.mail)
-                }
-                if (e.lat) {
-                  localStorage.setItem('driverLat', e.lat)
-                }
-                if (e.lng) {
-                  localStorage.setItem('driverLng', e.lng)
-                }
-                if (e.value) {
-                  localStorage.setItem('driverValue', e.value)
-                }
-                if (e.available) {
-                  localStorage.setItem('driverAvailable', e.available)
-                }
-                if (e.passengers) {
-                  localStorage.setItem('driverPassengers', JSON.stringify(e.passengers))
-                }
-              });
-              await this.loading.dismiss();
-              localStorage.setItem('sessionStatus', "true")
-              this.router.navigate(['/home']);
-              return
-            } else {
-              await this.loading.dismiss();
-              this.service.presentAlert("Usuario Incorrecto!");
-              return
-            }
-          }
-        }
-        this.service.presentAlert("Usuario Incorrecto", this.void);
-        this.tempLoginData.mail = "";
-        this.tempLoginData.name = "";
-        this.tempLoginData.boolean = false;
-        await this.loading.dismiss();
+      this.response = await Http.request({
+        method: 'GET',
+        url: 'https://nancyb3a.github.io/Test/usuarios_PGY4121_09.json'
       });
+      await this.loading.present();
+      // this.loginCheck(this.data.mail, this.data.password, this.response.data.alumnos)
+      this.loginData = this.login.loginCheck(this.data.mail, this.data.password, this.response.data.alumnos)
+      if (this.loginData.__zone_symbol__value.boolean == true) {
+        this.loginCheckData = {
+          mail: this.loginData.__zone_symbol__value.mail,
+          name: this.loginData.__zone_symbol__value.name,
+        }
+        this.firestore.getCollectionById<iUserData>("Users", this.loginCheckData.mail).pipe(take(1)).subscribe(e => {
+          if (!e) {
+            this.makeUserData.name = this.loginCheckData.name.substring(0, this.loginCheckData.name.indexOf(" "));
+            this.makeUserData.lName = this.loginCheckData.name.substring(this.loginCheckData.name.indexOf(" ") + 1);
+            this.makeUserData.mail = this.loginCheckData.mail;
+            this.firestore.setCollection("Users", this.loginCheckData.mail, this.makeUserData)
+            localStorage.setItem('userMail', this.loginCheckData.mail)
+            localStorage.setItem('userName', this.loginCheckData.name.substring(0, this.loginCheckData.name.indexOf(" ")))
+            localStorage.setItem('userLName', this.loginCheckData.name.substring(this.loginCheckData.name.indexOf(" ") + 1))
+          }
+          if (e.mail) {
+            localStorage.setItem('userMail', e.mail)
+          }
+          if (e.name) {
+            localStorage.setItem('userName', e.name)
+          }
+          if (e.lName) {
+            localStorage.setItem('userLName', e.lName)
+          }
+          if (e.number) {
+            localStorage.setItem('userNumber', e.number)
+          }
+          this.service.presentToast("Sesion Iniciada con el email: " + this.data.mail, 'middle');
+        })
+        this.firestore.getCollectionById<iDriverData>("Drivers", this.loginCheckData.mail).pipe(take(1)).subscribe(e => {
+          if (!e) {
+            this.firestore.setCollection("Drivers", this.loginCheckData.mail, this.makeDriverData)
+          }
+          if (e.vehicle) {
+            localStorage.setItem('driverVehicle', e.vehicle)
+          }
+          if (e.capacity) {
+            localStorage.setItem('driverCapacity', e.capacity)
+          }
+          if (e.patent) {
+            localStorage.setItem('driverPatent', e.patent)
+          }
+          if (e.time) {
+            localStorage.setItem('driverTime', e.time)
+          }
+          if (e.locationName) {
+            localStorage.setItem('driverLocationName', e.locationName)
+          }
+          if (e.name) {
+            localStorage.setItem('driverName', e.name)
+          }
+          if (e.lName) {
+            localStorage.setItem('driverLName', e.lName)
+          }
+          if (e.mail) {
+            localStorage.setItem('driverMail', e.mail)
+          }
+          if (e.lat) {
+            localStorage.setItem('driverLat', e.lat)
+          }
+          if (e.lng) {
+            localStorage.setItem('driverLng', e.lng)
+          }
+          if (e.value) {
+            localStorage.setItem('driverValue', e.value)
+          }
+          if (e.available) {
+            localStorage.setItem('driverAvailable', e.available)
+          }
+          if (e.passengers) {
+            localStorage.setItem('driverPassengers', JSON.stringify(e.passengers))
+          }
+        });
+        await this.loading.dismiss();
+        localStorage.setItem('sessionStatus', "true")
+        this.router.navigate(['/home']);
+      } else {
+        await this.loading.dismiss();
+        this.service.presentAlert("Usuario Incorrecto!");
+      }
+      // this.service.presentAlert("Usuario Incorrecto", this.void);
+      this.tempLoginData.mail = "";
+      this.tempLoginData.name = "";
+      this.tempLoginData.boolean = false;
+      await this.loading.dismiss();
+      // });
     } else {
       await this.loading.dismiss();
       this.service.presentAlert("Falta informacion en los siguientes campos: ", this.void);
